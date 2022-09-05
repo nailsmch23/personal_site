e20ad40bdff416462b2d6cf854dbca94c3f6be6d
Neil McHenry <neilmchenry@Neils-MacBook-Pro-2.local>
2022-09-05 02:04:38 -0700

    fixed background reference

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,19 +3,12 @@
 import '../stylesheets/Intro.css';
 
 // images
-import background_image from '../images/background.jpg'
 import me_image from '../images/me.jpg'
 
 function Intro() {
 
-    let intro_style = {
-        backgroundImage: 'url(' + background_image + ')',
-    };
-
     return (
         <div className="intro_container">
-            {/*<img src={background_image} alt='background image' className="intro_background_image" />*/}
-            {/*<div className="intro_background_overlay"></div>*/}
             <img src={me_image} alt='Neil' className="intro_me_image" />
             <div className="intro_inner_container">
                 <div className="intro_text">
