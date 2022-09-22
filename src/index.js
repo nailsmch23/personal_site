d13d6fde1811cc18c51e00d59bb801ddec0de9c8
Neil McHenry <neilmchenry@Neils-MacBook-Pro-2.local>
2022-09-22 00:38:51 -0700

    added google analytics tracking

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Base from "./components/Base";
 
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-242345977-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
